0249cf52a015a3825d17e951f95889f9941d13cc
Emeka Chukwulobe <emekabe@yahoo.com>
2020-10-02 23:54:35 +0100

    Firestore Recycler Adapter

---
+++ app/src/main/java/com/emekachukwulobe/cruddemo2/NoteAdapter.java
@@ -0,0 +1,53 @@
+package com.emekachukwulobe.cruddemo2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteHolder> {
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteHolder holder, int position, @NonNull Note model) {
+        holder.textViewTitle.setText(model.getTitle());
+        holder.textViewDescription.setText(model.getDescription());
+        holder.textViewPriority.setText(String.valueOf(model.getPriority()));
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+
+        return new NoteHolder(v);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+        TextView textViewTitle;
+        TextView textViewDescription;
+        TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
